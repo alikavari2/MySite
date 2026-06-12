48fcd4e9ef140c5dbb2c77b8acdb10d4d83accb1
alikavari2 <alikavarishirazu@gmail.com>
2026-06-13 01:44:48 +0330

    Create categoryMap.js

---
+++ src/data/categoryMap.js
@@ -0,0 +1,8 @@
+export const categoryMap = {
+  physics: 'فیزیک',
+  chemistry: 'شیمی',
+  biology: 'زیست‌شناسی',
+  'earth-astronomy': 'زمین و نجوم',
+  'computer-science': 'علوم کامپیوتر',
+  metaphysics: 'علوم فرامادی',
+};
